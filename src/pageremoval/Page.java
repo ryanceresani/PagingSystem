1316f8f96814c1689520a547d2138730a3c45de2
ryanceresani <ryanceresani@gmail.com>
2017-03-26 17:54:29 -0400

    page tweaks

--- src/pageremoval/Page.java
+++ src/pageremoval/Page.java
@@ -3,14 +3,11 @@ package pageremoval;
 import java.util.concurrent.atomic.AtomicInteger;
 
 public class Page {
-	boolean isAssigned;
 	int timesUsed;
 	private int id;
 	static AtomicInteger nextId = new AtomicInteger(0);
 
 	public Page(){
-		isAssigned = false;
-		timesUsed = 0;
 		this.id = nextId.incrementAndGet();
 	}
 
@@ -18,9 +15,6 @@ public class Page {
 		return id;
 	}
 
-	public void setAssigned(boolean a) {
-		isAssigned = a;
-	}
 	
 	public void printPage() {
 		String format = new String("| %7s | %2s");
